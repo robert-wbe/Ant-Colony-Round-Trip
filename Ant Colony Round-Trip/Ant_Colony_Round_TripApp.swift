c65dbd6b499f4b7666409305f2e8b5f08572be09
Robert Wiebe <robert.wiebe9@icloud.com>
2024-03-04 12:08:04 -0800

    Initial Commit

---
+++ Ant Colony Round-Trip/Ant_Colony_Round_TripApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ant_Colony_Round_TripApp.swift
+//  Ant Colony Round-Trip
+//
+//  Created by Robert Wiebe on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Ant_Colony_Round_TripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
