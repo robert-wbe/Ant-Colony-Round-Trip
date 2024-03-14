1e44a3036e506e77e380ae65a14a8ddad59670fe
Robert Wiebe <robert.wiebe9@icloud.com>
2024-03-14 10:16:44 -0700

    Omar is the best contributor to the project

--- Ant Colony Round-Trip/ContentView.swift
+++ Ant Colony Round-Trip/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, teammates!")
+            Text("Hello, teammates (especially Omar)!")
         }
         .padding()
     }
