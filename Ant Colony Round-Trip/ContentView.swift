519e0f232fc1742099f59e350720d359db476dcc
Robert Wiebe <robert.wiebe9@icloud.com>
2024-03-14 12:59:20 -0700

    first progress

--- Ant Colony Round-Trip/ContentView.swift
+++ Ant Colony Round-Trip/ContentView.swift
@@ -6,16 +6,26 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State var places: [CLLocation] = [CLLocation(latitude: 36.8127, longitude: -119.7466)]
+    @State var searchString: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, teammates (especially Omar)!")
-        }
-        .padding()
+            HStack {
+                TextField("Search cities", text: $searchString)
+                Button("Add place") {
+                    // put action here
+                }
+            }
+            Map() {
+                Annotation("Fresno State", coordinate: places[0].coordinate) {
+                    Circle()
+                }
+            }
+        }.padding()
     }
 }
 
